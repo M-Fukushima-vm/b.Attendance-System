8a6cfd3656b25575dcb86653af2d10fbe714d11b
M-Fukushima-vm <m.tech1980s@gmail.com>
2019-11-17 07:52:10 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ブラウザページタブ　表示情報をページごとに返す
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty?
+      base_title
+    else
+      page_name + " | " + base_title
+    end
+  end
 end
